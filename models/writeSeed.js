c6a158937e4d9542b8250b37a260485126f925a6
Anasemos Kassahun <anasemosaka@gmail.com>
2022-09-14 14:54:21 -0400

    updated seed data

--- models/writeSeed.js
+++ models/writeSeed.js
@@ -2,14 +2,14 @@ module.exports = [
     {
         name: 'Real Madrid',
         description: 'Home Real Madrid Jersey.',
-        img: 'https://images.unsplash.com/photo-1635710064268-69aa5d9355c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        img: 'https://images.media-arocam.com/GjX3cL3K2aIYGY6I7XdWh1mCia4=/fit-in/1000x1000/W28432/WHT/OuSmKqUj_WHT.png',
         price: 70,
         qty: 100
     },
     {
         name: 'Chelsea FC',
         description: 'Home Chelsea FC Jersey.',
-        img: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/b62dbf5d-11ee-4c63-995f-c80630157cd4/chelsea-fc-2021-22-stadium-home-mens-soccer-jersey-3BlHlg.png',
+        img: 'https://fanatics.frgimages.com/chelsea/mens-nike-blue-chelsea-2022/23-replica-home-jersey_pi4833000_ff_4833963-7266ee28a337f51f00bf_full.jpg?_hv=2',
         price: 70,
         qty: 100
     },
@@ -23,7 +23,7 @@ module.exports = [
     {
         name: 'Barcelona',
         description: 'Away Barcelona Jersey.',
-        img: 'https://www.footballshirtculture.com/images/2021/barcelona_2021_2022_away_kit.jpg',
+        img: 'https://cf.bestsoccerstore.com.cn/upload/ttmall/img/20220607/2ffdd3ce731d5204b01e9a4dd76262e1.png',
         price: 70,
         qty: 100
     },
@@ -58,14 +58,14 @@ module.exports = [
     {
         name: 'Newcastle United',
         description: 'Home Newcastle United Jersey.',
-        img: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiF0zsN84UnbGZq3xAG6H0QID_ldelS1_erC07xU7gYRnqa9p00Tqmal4U-XeQkdqyU8vQzx75wMlOKMqxT9LjyTZWIpdaZBFoXPabsDvQoUD9lgvGdITkQCy7-3u0yDFwtL3dM1UcthtDJHwFRBULBCL0uhDrcyNolR4HdcdUwavbQy_Lx-VgW0VeF/s600/newcastle-22-23-kit-3.jpg',
+        img: 'https://1.bp.blogspot.com/-QMUW_GxNAj8/YOl_Sie2DlI/AAAAAAADHiI/qbaHgZc2Fs4dGyZ_Pwpg0ZNeZQWFs2sJwCNcBGAsYHQ/s600/Castore%2BNewcastle%2BUnited%2B21-22%2BHome%2BKit%2B%25288%2529.jpg',
         price: 70,
         qty: 100
     },
     {
-        name: 'Borussia Dortmund',
-        description: 'Home Borussia Dortmund Jersey.',
-        img: 'https://www.protechkitzone.com/image/cache/data/2022_23_Jerseys/Bundesliga/BVB/BVB-22_23-Home-Shirt_Front-450x450.jpeg',
+        name: 'Ajax',
+        description: 'Home Ajax Jersey.',
+        img: 'https://www.soccerpro.com/wp-content/uploads/h58243_adidas_ajax_home_jsy_2022_23_01.jpg',
         price: 70,
         qty: 100
     },
@@ -93,14 +93,14 @@ module.exports = [
     {
         name: 'Atletico Madrid',
         description: 'Home Atletico Madrid Jersey.',
-        img: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhOQ3Z_ga0tYIX-jwq6F1AlrK8aPtXyoQNtQrkJ-8nv4G6ldqK3IviuRFETxCvRAwGuobo7fUMr8LT-gBBPDObnUG9k6Yj_dVzTo0c4MEzmD2ScvDTPZthVAUbBZWdR89WubE7iKvkrTYDkpI7VAW78WqHXPSIy9vkWlas4fVStXk9kvikdNgQgA4l06A/s600/Atl%C3%A9tico%20Madrid%2022-23%20Home%20&%20Away%20Kits%20%20%286%29.jpg',
+        img: 'https://1.bp.blogspot.com/-ngRm2koBM0c/YOLUU0LzI7I/AAAAAAACsxs/aCkSiTM_szMTHp8IT_6tRvrC5JRmlX1tgCLcBGAsYHQ/s600/atletico-21-22-kit-3.jpg',
         price: 70,
         qty: 100
     },
     {
         name: 'DC United',
         description: 'Home DC United Jersey.',
-        img: 'https://blogger.googleusercontent.com/img/a/AVvXsEhUMV1qO9F7421DTN6CsEtLAGG3MvhCj5RkUDeMMxwAys-X9zsxUG0t_gnwPyApan8FR1IAthCNx8kaSEWPyTuqNbBE3uGzTC2klIPAU3EoIdO0DHlof8nILPv6Rc9t9v67HEA6lwp8jRaIBf_AzmmB1wA2DVF3JqpWRe36tEAUuCdSxoeDa-r9HdDP=s600',
+        img: 'https://cdn.shopify.com/s/files/1/0360/2130/5482/products/D.C._United_20-21_Home_Jersey_White_GT2442_01_laydown_800x.jpg?v=1631580786',
         price: 70,
         qty: 100
     },
